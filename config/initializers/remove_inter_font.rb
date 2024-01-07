b5cb95cde1baa43904efc3c04a3ea9d484af51a2
Rob Zolkos <rob@zolkos.com>
2024-01-07 03:45:22 -0500

    Fix lint and test

--- config/initializers/remove_inter_font.rb
+++ config/initializers/remove_inter_font.rb
@@ -2,5 +2,5 @@
 # This is automatically included with tailwindcss-rails but unused in this project
 Rails.application.config.to_prepare do
   Rails.application.config.assets.precompile -= %w[inter-font.css]
-  Rails.application.config.assets.paths.reject! { _1.is_a?(String) && _1.include?('gems/tailwindcss-rails') }
+  Rails.application.config.assets.paths.reject! { _1.is_a?(String) && _1.include?("gems/tailwindcss-rails") }
 end
