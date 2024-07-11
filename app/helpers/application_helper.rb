e3f40872158f57e043d7af8921bd3f124d47d39e
aidewoode <aidewoode@gmail.com>
2024-07-11 15:41:51 +0800

    Add BlackCandy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def android_download_button(app_url)
+    case app_url
+    when /play\.google\.com/
+      render "download_button", path: app_url, icon: "googleplay", aria: "Link to Google Play Store"
+    when /f-droid\.org/
+      render "download_button", path: app_url, icon: "fdroid", aria: "Link to F-Droid"
+    end
+  end
 end
